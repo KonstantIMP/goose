5efa77ba286723798475b667027e6c28687a5371
KonstantinProgger <mihedovkos@gmail.com>
2020-04-18 13:31:45 +0300

    Crosplatform

--- src/option.cpp
+++ src/option.cpp
@@ -7,8 +7,10 @@
 #define CONFIG_FILE "config.json"
 
 #if defined(linux) || defined(__linux)
+    #define GLADE_API
     #define ERROR_MACROS(error_message) std::string("./error_msg " + std::string(error_message)).c_str()
 #else
+    #define GLADE_API __declspec(dllexport)
     #define ERROR_MACROS(error_message) std::string("error_msg.exe " + std::string(error_message)).c_str()
 #endif
 
@@ -23,7 +25,7 @@ GtkWidget * main_win;
 static GtkWidget * create_main_window();
 
 extern "C" {
-    CONFIGLIB_API void cancel() {
+    GLADE_API void cancel() {
         if(!cfg_json.is_config()) {
             system(ERROR_MACROS("\"You cannot play the game without option file!\""));
         }
@@ -31,30 +33,30 @@ extern "C" {
         gtk_main_quit();
     }
 
-    CONFIGLIB_API void ok() {
+    GLADE_API void ok() {
         cfg_json.write();
 
         gtk_main_quit();
     }
 
-    CONFIGLIB_API void change_check(GtkWidget * check, gpointer data) {
+    GLADE_API void change_check(GtkWidget * check, gpointer data) {
         if(std::string(gtk_label_get_text(GTK_LABEL(data))) == "VSync") {
             if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(check))) cfg_json.add_param("vsync", "en");
             else cfg_json.add_param("vsync", "dis");
         }
 
-        if(std::string(gtk_label_get_text(GTK_LABEL(data))) == "Music" || std::string(gtk_label_get_text(GTK_LABEL(data))) == "Музыка") {
+        if(std::string(gtk_label_get_text(GTK_LABEL(data))) == "Music" || std::string(gtk_label_get_text(GTK_LABEL(data))) == "\xd0\x9c\xd1\x83\xd0\xb7\xd1\x8b\xd0\xba\xd0\xb0") {
             if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(check))) cfg_json.add_param("music", "en");
             else cfg_json.add_param("music", "dis");
         }
 
-        if(std::string(gtk_label_get_text(GTK_LABEL(data))) == "Sound effects" || std::string(gtk_label_get_text(GTK_LABEL(data))) == "Звуковые эффекты") {
+        if(std::string(gtk_label_get_text(GTK_LABEL(data))) == "Sound effects" || std::string(gtk_label_get_text(GTK_LABEL(data))) == "\xd0\x97\xd0\xb2\xd1\x83\xd0\xba\xd0\xbe\xd0\xb2\xd1\x8b\xd0\xb5\x20\xd1\x8d\xd1\x84\xd1\x84\xd0\xb5\xd0\xba\xd1\x82\xd1\x8b") {
             if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(check))) cfg_json.add_param("sound", "en");
             else cfg_json.add_param("sound", "dis");
         }
     }
 
-    CONFIGLIB_API void lang_change(GtkWidget * combo) {
+    GLADE_API void lang_change(GtkWidget * combo) {
         if(gtk_combo_box_get_active(GTK_COMBO_BOX(combo)) == 0) cfg_json.add_param("language", "en");
         else cfg_json.add_param("language", "ru");
 
@@ -121,16 +123,16 @@ static GtkWidget * create_main_window() {
     gtk_window_set_title(GTK_WINDOW(window), "Options");
 
     gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(gtk_builder_get_object(window_builder, "lang_combo")), "0", "English");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(gtk_builder_get_object(window_builder, "lang_combo")), "1", "Русский");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(gtk_builder_get_object(window_builder, "lang_combo")), "1", "\xd0\xa0\xd1\x83\xd1\x81\xd1\x81\xd0\xba\xd0\xb8\xd0\xb9");
 
     if(cfg_json.is_config()) {
         if(cfg_json.get_param("language") == "ru") {
-            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "lang_msg")), "Язык");
+            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "lang_msg")), "\xd0\xaf\xd0\xb7\xd1\x8b\xd0\xba");
             gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "vsync_msg")), "VSync");
-            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "music_msg")), "Музыка");
-            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "sound_msg")), "Звуковые эффекты");
+            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "music_msg")), "\xd0\x9c\xd1\x83\xd0\xb7\xd1\x8b\xd0\xba\xd0\xb0");
+            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "sound_msg")), "\xd0\x97\xd0\xb2\xd1\x83\xd0\xba\xd0\xbe\xd0\xb2\xd1\x8b\xd0\xb5\x20\xd1\x8d\xd1\x84\xd1\x84\xd0\xb5\xd0\xba\xd1\x82\xd1\x8b");
 
-            gtk_button_set_label(GTK_BUTTON(gtk_builder_get_object(window_builder, "cancel_btn")), "Отмена");
+            gtk_button_set_label(GTK_BUTTON(gtk_builder_get_object(window_builder, "cancel_btn")), "\xd0\x9e\xd1\x82\xd0\xbc\xd0\xb5\xd0\xbd\xd0\xb0");
             gtk_button_set_label(GTK_BUTTON(gtk_builder_get_object(window_builder, "ok_btn")), "OK");
 
             gtk_combo_box_set_active(GTK_COMBO_BOX(gtk_builder_get_object(window_builder, "lang_combo")), 1);
@@ -157,12 +159,12 @@ static GtkWidget * create_main_window() {
         std::string lang = std::locale("").name();
 
         if(lang == "ru_RU.UTF-8") {
-            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "lang_msg")), "Язык");
+            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "lang_msg")), "\xd0\xaf\xd0\xb7\xd1\x8b\xd0\xba");
             gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "vsync_msg")), "VSync");
-            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "music_msg")), "Музыка");
-            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "sound_msg")), "Звуковые эффекты");
+            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "music_msg")), "\xd0\x9c\xd1\x83\xd0\xb7\xd1\x8b\xd0\xba\xd0\xb0");
+            gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(window_builder, "sound_msg")), "\xd0\x97\xd0\xb2\xd1\x83\xd0\xba\xd0\xbe\xd0\xb2\xd1\x8b\xd0\xb5\x20\xd1\x8d\xd1\x84\xd1\x84\xd0\xb5\xd0\xba\xd1\x82\xd1\x8b");
 
-            gtk_button_set_label(GTK_BUTTON(gtk_builder_get_object(window_builder, "cancel_btn")), "Отмена");
+            gtk_button_set_label(GTK_BUTTON(gtk_builder_get_object(window_builder, "cancel_btn")), "\xd0\x9e\xd1\x82\xd0\xbc\xd0\xb5\xd0\xbd\xd0\xb0");
             gtk_button_set_label(GTK_BUTTON(gtk_builder_get_object(window_builder, "ok_btn")), "OK");
 
             gtk_combo_box_set_active(GTK_COMBO_BOX(gtk_builder_get_object(window_builder, "lang_combo")), 1);
